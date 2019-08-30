a0e6e68fa0e3a672ba40cc576324dc5833572014
rbdanielp <54487691+rbdanielp@users.noreply.github.com>
2019-08-29 23:32:54 -0300

    Add files via upload

---
+++ DemoClass.java
@@ -0,0 +1,13 @@
+package edu;
+
+public class DemoClass {
+	
+	public String funcionSumarAB(int a, int b) {
+		return String.valueOf(a+b); 
+	}    
+	
+	public String saludo() {
+		System.out.println("HOLA");
+		return "HOLA";      
+	}
+}
